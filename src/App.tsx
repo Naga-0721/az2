5e9d991c38e8220121967b936720bb42038780ff
Srinivas <ksrinivasararao2001@gmail.com>
2023-04-24 14:50:21 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello this is Suraj
+        </p>
+        <a
+          className="App-link"
+          href="https://www.researchgate.net/publication/367067007_A_Study_on_Emergent_Augmented_Intelligence_Applications_in_Structural_Engineering"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Checkout my Research paper
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
