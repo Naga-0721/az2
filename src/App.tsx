7f644be4b3772ce882dc6498e676507048785c08
Srinivas <ksrinivasararao2001@gmail.com>
2023-04-24 14:52:53 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello this is Suraj
+          Hello this is Naga
         </p>
         <a
           className="App-link"
